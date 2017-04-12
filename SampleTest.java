f969bb59dfeca92ac699c4e3fff22e8d929c5635
AnbuSundar <anbu.sundara@gmail.com>
2017-04-12 14:46:12 -0700

    Add files via upload

---
+++ SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by anbus on 3/12/2017.
+ */
+public class SampleTest {
+    @Test
+    public void testCase1()
+    {
+        system.out.println("Hello");
+    }
+}
